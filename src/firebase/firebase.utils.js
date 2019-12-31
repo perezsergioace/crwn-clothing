983461731b9311e2f19af92fcd893afac5f3d3fb
perezsergioace <perez.sergio.ace@gmail.com>
2019-12-30 19:48:34 -0500

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,40 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyC_XyMjHXvs3UM7lUKul0_MG0_IQ9GYdk4",
-    authDomain: "crwn-db-dfacf.firebaseapp.com",
-    databaseURL: "https://crwn-db-dfacf.firebaseio.com",
-    projectId: "crwn-db-dfacf",
-    storageBucket: "crwn-db-dfacf.appspot.com",
-    messagingSenderId: "498375792852",
-    appId: "1:498375792852:web:c96d5d6edffcb00e594341",
-    measurementId: "G-2KYDBR7HNF"
+	apiKey: 'AIzaSyC_XyMjHXvs3UM7lUKul0_MG0_IQ9GYdk4',
+	authDomain: 'crwn-db-dfacf.firebaseapp.com',
+	databaseURL: 'https://crwn-db-dfacf.firebaseio.com',
+	projectId: 'crwn-db-dfacf',
+	storageBucket: 'crwn-db-dfacf.appspot.com',
+	messagingSenderId: '498375792852',
+	appId: '1:498375792852:web:c96d5d6edffcb00e594341',
+	measurementId: 'G-2KYDBR7HNF'
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) return;
+
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+    
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
 };
 
 firebase.initializeApp(config);
--- src/pages/signin-and-signup/SignIn-and-SignUp.js
+++ src/pages/signin-and-signup/SignIn-and-SignUp.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './signin-and-signup.scss';
 import SignIn from '../../components/signin/SignIn';
+import SingUp from '../../components/signup/SignUp';
 
 const SignInAndSignUpPage = () => (
     <div className='sign-in-and-sign-up'>
         <SignIn />
+        <SingUp />
     </div>
 )
 
